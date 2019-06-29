883b0c6f07c7671df9fc85b815ac78036b47450b
cgxqd <1661269978@qq.com>
2019-06-29 10:29:21 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <setup></setup>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    setup:()=>import('./components/setup.vue')
+  }
+}
+</script>
---
+++ src/components/setup.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{num}}</div>
+</template>
+<script>
+export default {
+    setup(){
+        return {
+            num:111
+        }
+    }
+}
+</script>
+
